d2597b33ca0e2cd9667508f0b1f8d6c57504ba10
Hamish MacDonald <elusivenode@gmail.com>
2023-11-14 09:23:50 +1000

    2020 day 1 prob 2

--- 2020/solutions/day1/day1.py
+++ 2020/solutions/day1/day1.py
@@ -5,6 +5,14 @@ def sum_to_something(l,s):
                 return (x,y)
     return (-1,-1)
 
+def sum_to_something_else(l,s):
+    for i,x in enumerate(l):
+        for y in l[i+1:]:
+            for z in l[i + 2:]:
+                if x + y + z == s:
+                    return (x,y,z)
+    return (-1,-1,-1)
+
 with open('/Users/hamish.macdonald/Dev/pyaoc/2020/puzzle_input/day1/input_1.txt', 'r') as input_f:
     input_lines = input_f.readlines()
 
@@ -14,3 +22,7 @@ for l in input_lines[:]:
 
 x,y = sum_to_something(expenses,2020)
 print(f'Answer to part 1: {x*y}')
+
+x,y,z = sum_to_something_else(expenses,2020)
+print(f'Answer to part 2: {x*y*z}')
+

a423b1c74c4b210cdfc86765bf882d5039c4ddc6
Hamish MacDonald <elusivenode@gmail.com>
2023-11-13 20:05:31 +1000

    day 1 prob 2

--- 2022/solutions/day1/day1_1_and_2.py
+++ 2022/solutions/day1/day1_1_and_2.py
@@ -7,6 +7,15 @@ def elf_max_cal(d):
             max_cal = d[k]
     return (d_key, max_cal)
 
+def get_cals_top_3(d):
+    all_cals = []
+    for k in d.keys():
+        all_cals.append(d[k])
+        all_cals.sort(reverse=True)
+
+    top_3_cals = sum(all_cals[0:3])
+    return top_3_cals
+
 with open('/Users/hamish.macdonald/Dev/pyaoc/2022/puzzle_input/day1/input_1.txt', 'r') as input_f:
     input_lines = input_f.readlines()
     elf_cals = {}
@@ -23,4 +32,5 @@ with open('/Users/hamish.macdonald/Dev/pyaoc/2022/puzzle_input/day1/input_1.txt'
 # add the last elf
 elf_cals[elf_no] = cal_ct
 
-print(elf_max_cal(elf_cals))
+print(f'Answer to part 1: {elf_max_cal(elf_cals)}')
+print(f'Answer to part 2: {get_cals_top_3(elf_cals)}')

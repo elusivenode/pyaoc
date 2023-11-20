f8f9801217b56a91f9a8b17b139cf1a13c5dd9b0
Hamish MacDonald <elusivenode@gmail.com>
2023-11-20 11:00:47 +1000

    start of 2020 day 7 part 2

--- 2020/solutions/day7/day7.py
+++ 2020/solutions/day7/day7.py
@@ -1,18 +1,29 @@
 import re
 
 def get_bags(b,d,chain):
+    #print(b)
+    #print(d[b])
     for i in d[b]:
+        #print(i)
+        i_rm_ct,_ = i.split(':')
+        #print(i_rm_ct)
         chain += f',{i}'
-        if i in d:
+        if i_rm_ct in d:
             #print(f'{i} {d[i]}')
-            chain = get_bags(i,d,chain)
+            chain = get_bags(i_rm_ct,d,chain)
     return chain
 
 def has_shiny_gold(chain):
     ct = 0
+    #print(chain)
     for b in chain.split(','):
-        if b == 'shiny gold':
-            ct += 1
+        #print(b)
+        if len(b) > 0:
+            bc,_ = b.split(':')
+            #print(bc)
+            if bc == 'shiny gold':
+                ct += 1
+                #print(ct)
     return ct
 
 with open('/Users/hamish.macdonald/Dev/pyaoc/2020/puzzle_input/day7/input_7.txt', 'r') as input_f:
@@ -24,7 +35,6 @@ for l in input_lines[:]:
     l = l.replace('bags', 'bag')
     l = l.replace('bag', '')
     l = l.replace('.', '')
-    l = re.sub(pattern, '', l)
     bag_rules_l.append(l.strip())
 
 bag_rules_d = {}
@@ -32,17 +42,34 @@ for rule in bag_rules_l:
     k,v = rule.split('contain')
     bags_l = []
     for bag in v.split(','):
-        bags_l.append(bag.strip())
+        match = re.search(pattern,bag)
+        n = '0'
+        if match:
+            n = str(match.group())
+        bag = re.sub(pattern, '', bag).strip() + ':' + n
+        bags_l.append(bag)
     bag_rules_d[k.strip()] = bags_l
 
 bag_chains = {}
 for k in bag_rules_d:
     bag_chain = ''
     bag_chains[k] = get_bags(k, bag_rules_d, bag_chain)
-
+#print(bag_chains)
 chains_with_shiny_gold = 0
 for k in bag_chains:
-     if has_shiny_gold(bag_chains[k]) > 0:
-         chains_with_shiny_gold += 1
+    if has_shiny_gold(bag_chains[k]) > 0:
+        chains_with_shiny_gold += 1
 
 print(f'The answer to part 1 is {chains_with_shiny_gold}')
+
+def ct_bags_within(root_bag, rules_d, multiplier, bag_ct):
+    for r in rules_d[root_bag]:
+        b, ct = r.split(':')
+        ct = int(ct)
+        multiplier *= multiplier
+        bag_ct += ct * multiplier
+        if b != 'no other':
+            bag_ct = ct_bags_within(b,rules_d, ct, bag_ct)
+    return bag_ct
+
+print(f'The answer to part 2 is {ct_bags_within("shiny gold", bag_rules_d, 1,0)}')
